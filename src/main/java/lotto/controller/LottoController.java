174b24b3964016cdffa176fd1707815d4afd6824
KUN <yoonkun1902@gmail.com>
2025-10-31 17:00:02 +0900

    feat(Controller): LottoController 로또 입력 기능 구현
    - 구매 금액 입력 받기
    - 로또 번호 입력 받기
    - 보너스 번호 입력 받기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,18 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
+import java.util.List;
+
 public class LottoController {
+
+    public void run() {
+
+        int purchaseAmount = InputView.getPurchaseAmount();
+        List<Integer> lottoNumbers = InputView.getLottoNumbers();
+        int bonusNumber = InputView.getBonusNumber();
+
+
+
+    }
 }
