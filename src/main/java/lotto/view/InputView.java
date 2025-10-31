8782031f66ad36b5599b6aecb1f8b906c315ce1a
KUN <yoonkun1902@gmail.com>
2025-10-31 17:05:33 +0900

    refactor(InputView): 입력 검증 예외 처리 방식 변경
    - getLottoNumbers: 검증 메서드들을 void로 변경하고 예외 발생
    - isValidCount: 개수 검증 실패 시 예외 발생
    - isValidRange: 범위 검증 실패 시 예외 발생
    - isNoDuplicate: 중복 검증 실패 시 예외 발생
    - getBonusNumber: 검증 결과에 따라 예외 발생
    - 검증은 view에서 실행하지 않도록
    Util패키지 아래 LottoValidator를 통한 입력값 검증로직 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,95 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import lotto.exceptions.ErrorMessage;
+import lotto.util.LottoValidator;
+import lotto.util.ValidationResult;
+
+
 public class InputView {
+    public static int getPurchaseAmount() {
+        while (true) {
+            try {
+                String purchaseAmount = Console.readLine();
+                ValidationResult result = LottoValidator.validatePurchaseAmount(purchaseAmount);
+
+                if (result.isValid()) {
+                    return Integer.parseInt(purchaseAmount) / 1000;
+                }
+                throw new IllegalArgumentException(result.getErrorMessage().getMessage());
+            } catch (IllegalArgumentException err) {
+                System.out.println(err.getMessage());
+            }
+        }
+    }
+
+    public static List<Integer> getLottoNumbers() {
+        while (true) {
+            try {
+                String input = Console.readLine();
+                List<Integer> numbers = parseLottoNumbers(input);
+                isValidLottoNumbers(numbers);
+                return numbers;
+            } catch (IllegalArgumentException err) {
+                System.out.println(err.getMessage());
+            }
+        }
+    }
+
+    private static List<Integer> parseLottoNumbers(String input) {
+        String[] inputs = input.split(",");
+        return Arrays.stream(inputs)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private static void isValidCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_COUNT.getMessage());
+        }
+    }
+
+    private static void isValidRange(List<Integer> numbers) {
+        for (Integer num : numbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE.getMessage());
+            }
+        }
+    }
+
+    private static void isNoDuplicate(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    private static void isValidLottoNumbers(List<Integer> numbers) {
+        isValidCount(numbers);
+        isValidRange(numbers);
+        isNoDuplicate(numbers);
+    }
+
+    public static int getBonusNumber() {
+        while (true) {
+            try {
+                String bonusInput = Console.readLine();
+                ValidationResult result = LottoValidator.inputNumberImplement(bonusInput);
+
+                if (result.isValid()) {
+                    return Integer.parseInt(bonusInput);
+                }
+                throw new IllegalArgumentException(result.getErrorMessage().getMessage());
+            } catch (IllegalArgumentException err) {
+                System.out.println(err.getMessage());
+            }
+        }
+    }
 }
+
+
