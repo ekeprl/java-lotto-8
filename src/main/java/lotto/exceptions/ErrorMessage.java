918796db234a292eb9ff1beb3a98c9ec9929d2c6
KUN <yoonkun1902@gmail.com>
2025-11-03 15:16:54 +0900

    refactor(ErrorMessage): 에러 메시지 문구 개선 - DUPLICATE_NUMBER 메시지수정 :  '중복된 번호가 있습니다.' → '로또 번호는 중복될 수 없습니다.'

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.exceptions;
 public enum ErrorMessage {
     INVALID_LOTTO_COUNT("로또 번호는 6개를 입력해야 합니다."),
     OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATE_NUMBER("중복된 번호가 있습니다."),
+    DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
     INVALID_INPUT_FORMAT("입력 형식이 올바르지 않습니다."),
     INVALID_PURCHASE_AMOUNT("입력 가격이 올바르지 않습니다."),
     BONUS_NUMBER_DUPLICATE("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
