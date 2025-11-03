59609e389cefa43be2e7ae5da5b40bdd869a8628
KUN <yoonkun1902@gmail.com>
2025-11-03 14:34:23 +0900

    chore(ErrorMessage): 로또 도메인 검증을 위한 에러 메시지 추가
    - BONUS_NUMBER_DUPLICATE: 보너스 번호 중복 검증"

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER("중복된 번호가 있습니다."),
     INVALID_INPUT_FORMAT("입력 형식이 올바르지 않습니다."),
-    INVALID_PURCHASE_AMOUNT("입력 가격이 올바르지 않습니다.");
+    INVALID_PURCHASE_AMOUNT("입력 가격이 올바르지 않습니다."),
+    BONUS_NUMBER_DUPLICATE("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+
 
     private final String message;
 
