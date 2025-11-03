7cc60bb830fe831c0d70f34b07d64635bf457d38
KUN <yoonkun1902@gmail.com>
2025-11-03 17:01:13 +0900

    refactor(LottoController): 로또 게임 로직 구현
    - 입력값 이후 로또 구매 기능 추가
    - 당첨 번호 입력 및 당첨 판정 로직 추가
    - 당첨 통계 및 수익률 계산 연동
    - Model 클래스들(LottoTickets, WinningLotto, LottoResult)과 통합, 연동

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,18 +1,44 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.*;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LottoController {
 
     public void run() {
-
+        // 구매 금액 입력 및 로또 번호 입력
         int purchaseAmount = InputView.getPurchaseAmount();
+        LottoTickets lottoTickets = generateLottos(purchaseAmount);
+        OutputView.printLottos(lottoTickets);
+
+        // 당첨 번호
         List<Integer> lottoNumbers = InputView.getLottoNumbers();
         int bonusNumber = InputView.getBonusNumber();
+        DrawingLotto winningLotto = new DrawingLotto(
+                new Lotto(lottoNumbers), bonusNumber);
 
+        // 통계 계산
+        Map<Rank, Integer> rankCount = lottoTickets.match(winningLotto);
+        LottoResult result = new LottoResult(rankCount, purchaseAmount);
 
+        OutputView.printResult(result);
+    }
 
+    private LottoTickets generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return new LottoTickets(lottos);
     }
 }
